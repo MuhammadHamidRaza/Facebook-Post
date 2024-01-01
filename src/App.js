f09ffcbec08e322a0945a5210cbc72fa33c0c3da
MuhammadHamidRaza <hr1907469@gmail.com>
2024-01-01 19:24:01 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import FbImageLibrary from 'react-fb-image-grid';
 import './App.css';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  function getData() {
+    fetch('https://dummyjson.com/products')
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res.products);
+        setProducts(res.products);
+      });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 style={{ textAlign: 'center' }}>Post</h1>
+        {products.length &&
+          products.map(function (item, index) {
+            return (
+              <div>
+                <h1>{item.title}</h1>
+                <h3>{item.brand}</h3>
+                <br/>
+                <p>{item.description}</p>
+
+                {item.images && <FbImageLibrary images={item.images} />}
+                <div style={{textAlign:"center",display:'flex',justifyContent:'space-around'}} >
+                  <div>
+                  <img style={{width :"25%",height:"55%",}} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwUjSN0nxSfH5bbLbba5lzGaq4t6wgXx5ALA&usqp=CAU'></img><p>Like</p>
+                  </div>
+                  <div>
+                  <img style={{width :"25%",height:"55%",}} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5uOFm8Gqpi4N6pjZOUOiSSI0nNEC6NSWPKw&usqp=CAU'></img><p>Comment</p>
+                  </div>
+                  <div>
+                  <img style={{width :"25%",height:"55%",}} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOXvuBh44EUYz1kzTUWYcqz-pxBwsVtfNlaQ&usqp=CAU'></img><p>Comment</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
       </header>
     </div>
   );
